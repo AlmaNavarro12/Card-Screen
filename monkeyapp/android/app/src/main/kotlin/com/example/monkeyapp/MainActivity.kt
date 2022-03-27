10bb268c9cbf3618922a427b679872133baa3b5b
Alma Navarro <97627994+AlmaNavarro12@users.noreply.github.com>
2022-03-26 22:11:12 -0600

    Delete monkeyapp directory

--- monkeyapp/android/app/src/main/kotlin/com/example/monkeyapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.monkeyapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- monkeyapp/lib/routes/routes.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import '../pages/login_page.dart';
-
-
-final route = <String, WidgetBuilder>{
-  '/' : (BuildContext context) => const LoginPage(),
-};
